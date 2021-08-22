da036a6992c869a376e3cd952d500ec32930fde6
Ahmed Ramy <ahmedramy.e404@gmail.com>
2021-08-22 15:58:40 +0200

    finished code

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ var menu = document.getElementById("menu");
 var nav = document.getElementById("nav")
 var exit = document.getElementById("exit")
 
+
 menu.addEventListener("click", function (e) {
     nav.classList.toggle("hide-mobile");
     e.preventDefault();
